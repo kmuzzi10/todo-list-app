aff6cbae31b8b9fed715c861d28a8bea6fb70a53
kmuzzi10 <kmuzammil12546@gmail.com>
2023-09-05 02:40:59 +0500

    complete

--- app.js
+++ app.js
@@ -6,9 +6,9 @@ import mongoose from "mongoose";
 //mongo db
 mongoose.connect("mongodb://127.0.0.1:27017/todolistDB");
 const itemsSchema = mongoose.Schema({
-  name:String
+  name: String
 })
-const Item = mongoose.model("item",itemsSchema)
+const Item = mongoose.model("item", itemsSchema)
 
 const app = express();
 
@@ -16,24 +16,31 @@ const app = express();
 //express
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-   //mongodb
-   const item1 = {
-    name: "welcome to todo list"
-  }
-  const item2 = {
-    name: "hit the + button to add todos"
-  }
-  const item3 = {
-    name: "hit checkbox to delete items"
-  }
-  const defaultItem = [item1,item2,item3]
-  
+//mongodb
+const item1 = {
+  name: "welcome to todo list"
+}
+const item2 = {
+  name: "hit the + button to add todos"
+}
+const item3 = {
+  name: "hit checkbox to delete items"
+}
+const defaultItem = [item1, item2, item3]
+
+const listSchema = mongoose.Schema({
+  name:String,
+  items:[itemsSchema]
+})
+
+const List = mongoose.model("List",listSchema)
 
 
-app.get("/", async function(req, res) {
+
+app.get("/", async function (req, res) {
 
   const date = new Date();
 
@@ -43,50 +50,113 @@ app.get("/", async function(req, res) {
 
   let currentYear = date.getFullYear();
   let day = `${currentDay}-${currentMonth}-${currentYear}`;
-  
 
-//mongo
-  const items =await Item.find({}).exec();
 
-  if(items.length === 0){
+  //mongo
+  const items = await Item.find({}).exec();
+
+  if (items.length === 0) {
     Item.insertMany(defaultItem);
     res.redirect("/")
-  }else{
-    res.render("list", {listTitle: day, newListItems: items});
+  } else {
+    res.render("list", { listTitle: "Today", newListItems: items });
   }
 
 
 });
 
-app.post("/", function(req, res){
+app.get("/:customListName",async (req,res)=>{
+   const customListName = req.params.customListName;
+  const ab = req.params.customListName;
+  try {
+    const foundList = await List.findOne({ name: customListName });
+
+     if(ab == "about"){
+      res.render("about.ejs")
+    }
+    if (!foundList) {
+      //create new list
+      const list = new List({
+        name:customListName,
+        items:defaultItem
+      });
+      list.save();
+      res.redirect("/"+customListName)
+    }
+     else {
+      //show existing list
+      res.render("list",{ listTitle: foundList.name, newListItems: foundList.items })
+    }
+  } catch (err) {
+    console.error(err);
+  }
+  
+  
+})
+
+app.post("/",async function (req, res) {
 
   const itemName = req.body.newItem;
-  
+  const listName = req.body.list;
+
   const item = new Item({
-    name:itemName
+    name: itemName
   });
+  if(listName==="Today"){
+    item.save();
+    res.redirect("/")
+  }else {
+    try {
+      const foundList = await List.findOne({ name: listName });
+      if (foundList) {
+        foundList.items.push(item);
+        await foundList.save();
+        res.redirect("/" + listName);
+      } else {
+        console.log("List doesn't exist");
+        // Handle the case where the list doesn't exist
+      }
+    } catch (err) {
+      console.error(err);
+      // Handle the error appropriately
+    }
+  }
+  
 
-  item.save();
-  res.redirect("/")
-
-
-//   if (req.body.list === "Work") {
-//     workItems.push(item);
-//     res.redirect("/work");
-//   } else {
-//     items.push(item);
-//     res.redirect("/");
-//   }
+  
 });
+app.post("/delete",async (req,res)=>{
+  const checkBoxId = req.body.checkbox;
+  const listName = req.body.listName;
+  if(listName == "Today"){
+    await Item.deleteOne({_id: checkBoxId});
+    res.redirect("/")
+  }else {
+    try {
+      const foundList = await List.findOne({ name: listName });
+      if (foundList) {
+        const updatedList = await List.findOneAndUpdate(
+          { name: listName },
+          { $pull: { items: { _id: checkBoxId } } }
+        );
+        if (updatedList) {
+          res.redirect("/" + listName);
+        } else {
+          console.log("List not updated");
+        }
+      } else {
+        console.log("List doesn't exist");
+        // Handle the case where the list doesn't exist
+      }
+    } catch (err) {
+      console.error(err);
+      // Handle the error appropriately
+    }
+  }
+  
+  
+})
 
-app.get("/work", function(req,res){
-  res.render("list", {listTitle: "Work List", newListItems: workItems});
-});
-
-app.get("/about", function(req, res){
-  res.render("about");
-});
-
-app.listen(3000, function() {
+app.listen(3000, function () {
   console.log("Server started on port 3000");
 });
